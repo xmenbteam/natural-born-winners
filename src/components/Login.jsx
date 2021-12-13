7dbabae8dcd3cbd3fe6ff456e6f75a9a1096596a
two-triangles <danielsalehguitar@gmail.com>
2021-12-13 19:33:01 +0000

    add firebase cloud database

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,59 @@
+import React, { useState } from "react"
+import { collection, addDoc } from "firebase/firestore"
+import db from "../firebase/firebase"
+
 const Login = () => {
+  const [username, setUsername] = useState("")
+  const [company, setCompany] = useState("")
+  const [email, setEmail] = useState("")
+
   return (
     <form className="inputs">
-      <input name="username" placeholder="Username" className="form-input" />
-      <input name="company" placeholder="Company" className="form-input" />
-      <input name="email" placeholder="Email" className="form-input" />
+      <input
+        name="username"
+        placeholder="Username"
+        className="form-input"
+        onChange={(event) => {
+          setUsername(event.target.value)
+        }}
+        value={username}
+      />
+      <input
+        name="company"
+        placeholder="Company"
+        className="form-input"
+        onChange={(event) => {
+          setCompany(event.target.value)
+        }}
+        value={company}
+      />
+      <input
+        name="email"
+        placeholder="Email"
+        className="form-input"
+        onChange={(event) => {
+          setEmail(event.target.value)
+        }}
+        value={email}
+      />
+      <button
+        onClick={async (event) => {
+          event.preventDefault()
+          await addDoc(collection(db, "users"), {
+            username: username,
+            company: company,
+            email: email,
+          })
+          setUsername("")
+          setCompany("")
+          setEmail("")
+        }}
+        className="enter-button"
+      >
+        Begin
+      </button>
     </form>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
